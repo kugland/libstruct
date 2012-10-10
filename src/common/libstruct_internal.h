8e354ab25a87c936af5a3ea6631a6d368fbfcc31
André von Kugland <kugland@gmail.com>
2012-10-10 20:37:21 -0300

    Created “libstruct_internal.h”.

---
+++ src/common/libstruct_internal.h
@@ -0,0 +1,20 @@
+#ifndef LIBSTRUCT_INTERNAL_H_INCLUDED
+#define LIBSTRUCT_INTERNAL_H_INCLUDED
+
+#ifdef __GNUC__
+#define likely(x)       __builtin_expect((x),1)
+#define unlikely(x)     __builtin_expect((x),0)
+#else
+#define likely(x)       (x)
+#define unlikely(x)     (x)
+#endif
+
+#if defined(__GNUC__) && ((__GNUC__ > 3) || (__GNUC__ == 3 && __GNUC_MINOR__ >= 1))
+#define INLINE inline __attribute__((always_inline))
+#elif defined(_MSC_VER)
+#define INLINE __forceinline
+#else
+#define INLINE inline
+#endif
+
+#endif
