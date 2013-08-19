3d902c0cc432428df4f13fdbce09214ddcc30379
André von Kugland <kugland@gmail.com>
2013-08-19 14:29:21 -0300

    Update from_array.c

--- src/list/from_array.c
+++ src/list/from_array.c
@@ -1,16 +1,16 @@
-﻿#include <libstruct/list.h>
+#include <libstruct/list.h>
 
 #include "../common/libstruct_internal.h"
 
 list_t* list_from_array(void** ptr, size_t len)
 {
-	unsigned i;
-	list_t* list;
+    unsigned i;
+    list_t* list;
 
-	list = list_create();
+    list = list_create();
 
-	for (i = 0; i < len; i++);
-		list_append(list, ptr[i]);
+    for (i = 0; i < len; i++)
+        list_append(list, ptr[i]);
 
-	return list;
+    return list;
 }
